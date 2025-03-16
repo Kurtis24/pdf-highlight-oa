7a2de657f3e786f5b061eacece738de43256203c
KurtisL7 <kurtislin24@gmail.com>
2025-03-15 22:30:08 -0400

    Starting

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<HeaderProps> = ({}: HeaderProps) => {
     <div className="w-full bg-white shadow-md p-4 flex items-center justify-center md:flow-root">
       <div className="float-left h-full">
         <h1 className="text-2xl font-bold underline underline-offset-8">
-          Adanomad Challenge
+          Adanomad
         </h1>
       </div>
       {/*<div className="float-right space-x-2">
